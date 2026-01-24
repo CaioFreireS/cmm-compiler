43ee16bdbfc6b6e6d2d18cc0d587ddc8f618e176
CaioFreireS <caioFS23@hotmail.com>
2026-01-24 15:56:40 -0300

    Add initial project structure with Maven configuration and basic classes

---
+++ src/main/java/com/mycompany/cmm/compiler/CmmCompiler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cmm.compiler;
+
+/**
+ *
+ * @author caiof
+ */
+public class CmmCompiler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/cmm/compiler/model/Token.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cmm.compiler.model;
+
+/**
+ *
+ * @author caiof
+ */
+public class Token {
+    
+}
